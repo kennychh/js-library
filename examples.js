52f1fdec8f04b929beec9c9575f1ae52074532f5
Kenny <kennyhy.chan@mail.utoronto.ca>
2020-11-22 00:21:55 -0500

    rain test

---
+++ examples.js
@@ -0,0 +1,4 @@
+"use strict";
+
+const r = new Rain()
+r.makeItRain()
---
+++ weather.js
@@ -0,0 +1,25 @@
+"use strict";
+function randRange(min, max) {
+  return (Math.floor(Math.random()*(min-max+1))+min);
+}
+
+function Rain () {
+  this.raindrops = []
+}
+Rain.prototype = {
+
+  makeItRain: function () {
+    for (var i = 1; i < 500; i++){
+      var dropLeft = randRange(0, 1600)
+      var dropTop =  randRange(-1000, 1400)
+    }
+    const raindrop = document.createElement('div')
+    raindrop.className ="drop"
+    raindrop.id = 'drop' + i
+    $('#drop'+i).css('left', dropLeft)
+    $('#drop'+i).css('top', dropTop)
+    const body = $('body')
+    body.append(raindrop)
+    this.raindrops.push(raindrop)
+  }
+}
