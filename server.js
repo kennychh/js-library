53ee5acaea19457ef5270ad0bbafae72037e9c19
Kenny <kennyhy.chan@mail.utoronto.ca>
2020-11-25 16:57:12 -0500

    test

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 	//res.send('This should be the root route!')
 
 	//sending some HTML
-	res.sendFile('/course.html');
+	res.sendFile('weather.html',{root:__dirname+'/pub'});
 
 })
 
