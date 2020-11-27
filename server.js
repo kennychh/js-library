9d3f8f8bda6f11692890e2097bedb58d16567fb1
Kenny <kennyhy.chan@mail.utoronto.ca>
2020-11-26 19:52:48 -0500

    examples

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 	//res.send('This should be the root route!')
 
 	//sending some HTML
-	res.sendFile('weather.html',{root:__dirname+'/pub'});
+	res.sendFile('examples.html',{root:__dirname+'/pub'});
 
 })
 
