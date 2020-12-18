8ef9eacd9a471c7b6a11a3dd049b88469877cf52
Kenny <kennyhy.chan@mail.utoronto.ca>
2020-12-18 17:57:00 -0500

    row1

--- pub/landing.js
+++ pub/landing.js
@@ -5,9 +5,12 @@ const sn = new Snow()
 const sbox = s.makeSunMoon(true, false)
 sbox.className = 'sbox'
 const smbox = s.makeNightDay(false,false)
-smbox.id = 'rightbox'
 smbox.className = 'sbox'
 const snbox = sn.snow(100, false)
 snbox.className = 'box'
 const rbox = r.makeItRain(false)
 rbox.className = 'box'
+
+const row1 = document.createElement('div')
+row1.append(sbox)
+row1.append(smbox)
