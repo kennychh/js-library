ce4ca71f95c84613c55c157ab100b76a58bb7958
Kenny <kennyhy.chan@mail.utoronto.ca>
2020-12-18 17:37:44 -0500

    s

--- pub/landing.js
+++ pub/landing.js
@@ -3,10 +3,6 @@ const s = new Sun()
 const sn = new Snow()
 
 const sbox = s.makeSunMoon(false, false)
-sbox.className = 'sbox'
 const smbox = s.makeNightDay(false,false)
-smbox.className = 'sbox'
 const snbox = sn.snow(100, false)
-snbox.className = 'box'
 const rbox = r.makeItRain(false)
-rbox.className = 'box'
