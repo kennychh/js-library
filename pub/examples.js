0d3602f4b978a81d81026c807e4ba483f6bd6936
Kenny <kennyhy.chan@mail.utoronto.ca>
2020-12-18 17:04:10 -0500

    s

--- pub/examples.js
+++ pub/examples.js
@@ -4,18 +4,15 @@ const r = new Rain()
 const s = new Sun()
 const sn = new Snow()
 
-const makeSunMoonCode = document.createElement('div')
-makeSunMoonCode.id = "makeSunMoonCode"
 const smImg = document.createElement('IMG')
 smImg.id = 'smImg'
 smImg.src = "images/makeSunMoonCode.png"
-$('#makeSunMoonCode').html(smImg)
 
 
 s.makeSunMoon(false, false)
 addTitle('Sun and Moon')
-$('body').append(makeSunMoonCode)
-s.makeNightDay(makeSunMoonCode)
+$('body').append(smImg)
+s.makeNightDay()
 addTitle('Night and Day')
 sn.snow(50)
 addTitle('Snow')
