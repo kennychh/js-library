bdbfc0e9cc4487ed1a09a2c7e65b3f71711c6cf6
Kenny <kennyhy.chan@mail.utoronto.ca>
2020-12-18 16:12:32 -0500

    s

--- pub/examples.js
+++ pub/examples.js
@@ -3,7 +3,7 @@
 const r = new Rain()
 const s = new Sun()
 const sn = new Snow()
-s.makeSunMoon(isItCloudy = false)
+s.makeSunMoon(false, false)
 addTitle('Sun and Moon')
 s.makeNightDay()
 addTitle('Night and Day')
