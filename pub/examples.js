602c963745ad6495555200a080cb4dfaaf230b41
Kenny <kennyhy.chan@mail.utoronto.ca>
2020-12-18 17:00:14 -0500

    s

--- pub/examples.js
+++ pub/examples.js
@@ -8,8 +8,10 @@ const makeSunMoonCode = document.createElement('div')
 makeSunMoonCode.id = "makeSunMoonCode"
 const smImg = document.createElement('IMG')
 smImg.id = 'smImg'
-smImg.src = "./images/makeSunMoonCode.png"
+smImg.src = "images/makeSunMoonCode.png"
 $('#makeSunMoonCode').html(smImg)
+
+
 s.makeSunMoon(false, false)
 addTitle('Sun and Moon')
 $('body').append(makeSunMoonCode)
